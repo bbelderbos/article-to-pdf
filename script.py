8d6ef5838f4ab5319ad77dca45c99b0692d4066f
Bob Belderbos <bobbelderbos@gmail.com>
2024-03-20 16:01:56 +0100

    use constant

--- script.py
+++ script.py
@@ -7,13 +7,13 @@ import requests
 
 ARTICLE_ENDPOINT = "https://codechalleng.es/api/articles/"
 OUTPUT_DIR = Path("outputs")
+HEADERS = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3"
+}
 
 
 def fetch_article_content(url):
-    headers = {
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3"
-    }
-    response = requests.get(url, headers=headers)
+    response = requests.get(url, headers=HEADERS)
     return response.text
 
 
